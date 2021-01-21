6103fabcc44af9a461a46d3d5c4cd6545c30cc6c
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-01-21 11:21:51 +0530

    project completed

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
 import carConfiguration from "./carConfiguration"
 
 import Car from "./car"
+import engineConfiguration from "./engineConfiguration"
 
-// let carObj:carConfiguration = {
-//     barndValue:"Tesla",
-//     modelValue:"model 3",
-//     engineValue:"battery",
-//     speedValue:160,
-//     milageValue:500,
-//     doorsValue:"SIX"
-// }
-// let engineObj = {
-//     fuelType:"BATTERY",
-//     rpm:700,
-//     torque:1000,
-//     cylinders:4
-// }
 
-// let car = new Car(carObj,engineObj)
-
-// car.getData()
-// car.getEngineData()
 
 
 
@@ -28,14 +11,26 @@ import Car from "./car"
     let barndValue = (<HTMLInputElement>document.getElementById("barndValue")).value
     let modelValue = (<HTMLInputElement>document.getElementById("modelValue")).value
     let engineValue = (<HTMLInputElement>document.getElementById("engineValue")).value
-    let speedValue = (<HTMLInputElement>document.getElementById("speedValue")).value
-    let milageValue = (<HTMLInputElement>document.getElementById("milageValue")).value
+    let speedValue = +((<HTMLInputElement>document.getElementById("speedValue")).value)
+    let milageValue = +((<HTMLInputElement>document.getElementById("milageValue")).value)
     let doorsValue = (<HTMLSelectElement>document.getElementById("doorsValue")).value
 
     let fuelType = (<HTMLSelectElement>document.getElementById("fuelType")).value
-    let rpm = (<HTMLInputElement>document.getElementById("rpm")).value
-    let torque = (<HTMLInputElement>document.getElementById("torque")).value
-    let cylinders = (<HTMLSelectElement>document.getElementById("cylinders")).value
+    let rpm = +((<HTMLInputElement>document.getElementById("rpm")).value)
+    let torque = +((<HTMLInputElement>document.getElementById("torque")).value)
+    let cylinders = +((<HTMLSelectElement>document.getElementById("cylinders")).value)
 
-    console.log(barndValue,modelValue,engineValue,speedValue,milageValue,doorsValue,fuelType,rpm,torque,cylinders)
+    // console.log(barndValue,modelValue,engineValue,speedValue,milageValue,doorsValue,fuelType,rpm,torque,cylinders)
+
+    let carObj:carConfiguration = {
+        barndValue,modelValue,engineValue,speedValue,milageValue,doorsValue
+    }
+    let engineObj:engineConfiguration = {
+        fuelType,rpm,torque,cylinders
+    }
+    
+    let car = new Car(carObj,engineObj)
+    
+    car.getData()
+    car.getEngineData()
 })
